2ecc7a7cc1c3dd6f1831d99cb7e20d4010af12d6
Mayra <alejandra_perez_4@hotmail.com>
2018-12-01 17:28:48 -0600

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,38 @@
 import os
 import csv
 
-csvpath=os.path.join('.','PyBank', 'budget_data.csv')
-with open(csvpath, newline='') as csvfile:
+Data=os.path.join('..','PyBank', 'budget_data.csv')
+Date=[]
+PL=[]
+Change=[]
+
+with open(Data, newline='') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     csv_header=next(csvreader)
-    print(f"{csv_header}")
     for row in csvreader:
-        print(row)#first 2 rows
+        Date.append(row[0])
+        PL.append(int(row[1]))
+        
+Data_Cleaned=zip(Date,PL)
+print (f"Total Months: {len(Date)}")
+def fsum(List):
+    Sum = 0
+    for i in List:
+        Sum = Sum + i
+    return Sum
+print (f"Total: {fsum(PL)}")
+Change=[PL[i+1]-PL[i] for i in range(len(PL)-1)]
+import statistics
+print (f"Average  Change: {round(statistics.mean(Change),2)}")
+print (f"Greatest Increase in Profits: {Date[Change.index(max(Change))+1]} {max(Change)}")
+print (f"Greatest Decrease in Profits: {Date[Change.index(min(Change))+1]} {min(Change)}")
+
+
+
+output_file=os.path.join("budget_final.txt")
+with open(output_file, "w", newline="\n") as text_file:
+    text_file.writelines(f"Total Months: {len(Date)}\n")
+    text_file.writelines(f"Total: {fsum(PL)}\n")
+    text_file.write(f"Average  Change: {round(statistics.mean(Change),2)}\n")
+    text_file.write(f"Greatest Increase in Profits: {Date[Change.index(max(Change))+1]} {max(Change)}\n")
+    text_file.write(f"Greatest Decrease in Profits: {Date[Change.index(min(Change))+1]} {min(Change)}\n")
